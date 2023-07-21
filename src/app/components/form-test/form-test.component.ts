b56c5b95abaa9e1807f62e210b3b37644840425c
Komiljon Najmitdinov <komiljonnajmitdinov99@gmail.com>
2023-07-21 17:25:30 +0500

    form changees

--- src/app/components/form-test/form-test.component.ts
+++ src/app/components/form-test/form-test.component.ts
@@ -32,38 +32,21 @@ export class FormTestComponent implements OnInit {
     return this.form.get('complexForm') as FormArray;
   }
 
-  get oneLevelForm(): FormGroup {
-    return this.complexForm.get('oneLevel') as FormGroup;
-  }
 
-
-  get multiLevelForm(): FormArray {
-    return this.complexForm.get('multiLevel') as FormArray;
+  initComplexForm() {
+     
   }
 
 
   /// buttons
 
   addOneLevelForm() {
-    this.complexForm.push(this.formBuilder.group({
-      oneLevel: this.formBuilder.group({
-        column: [''],
-      }),
-      multiLevel: this.formBuilder.group([])
-    }));
-    const form = this.complexForm.controls[0];
-    console.log(form as FormArray);
+  
     
   }
 
   addMultiLevelForm() {
-    console.log(this.multiLevelForm);
-    
-    this.multiLevelForm.push(
-      this.formBuilder.group({
-        column: ['']
-      })
-    )
+   
   }
 
   
